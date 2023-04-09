ecc6beacc57fb612f3ec14eace68b4b2ae49a479
magali <maga_orellana02@hotmail.com>
2023-04-09 00:22:07 -0300

    cuarto commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
@@ -8,6 +9,11 @@ const Navbar = () => {
     <nav>
       {/* Aqui deberan agregar los liks correspondientes a las rutas definidas */}
       {/* Deberan implementar ademas la logica para cambiar de Theme con el button */}
+      <ul>
+        <li><Link to="/home">Home</Link></li>
+        <li><Link to="/favs">Favoritos</Link></li>
+        <li><Link to="contact">Contacto</Link></li>
+      </ul>
       <button>Change theme</button>
     </nav>
   )
